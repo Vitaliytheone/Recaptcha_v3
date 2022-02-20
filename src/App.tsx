bcc5aae038cfd4493ae866502997aab3df9e23e5
VITALIJ <Vitaliythesecond@gmail.com>
2022-02-21 00:43:16 +0300

    added submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Comp from "./components";
 
 function App() {
-    // const SITE_KEY = "6LfYm48eAAAAALijNLawLq5AmM_-FdBaaqTzNlQV";
+    const SITE_KEY = "6LfYm48eAAAAALijNLawLq5AmM_-FdBaaqTzNlQV";
 
-    // useEffect(() => {
-    //     const script = document.createElement("script");
-    //     script.src = "https://www.google.com/recaptcha/api.js?render=" + SITE_KEY;
-    //     document.body.appendChild(script);
-    // }, []);
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://www.google.com/recaptcha/api.js?render=" + SITE_KEY;
+        document.body.appendChild(script);
+    }, []);
 
     return (
         <div className="App">
-            <Comp />
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
+                <button>Submit recaptcha</button>
             </header>
         </div>
     );
