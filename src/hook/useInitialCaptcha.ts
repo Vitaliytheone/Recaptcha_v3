a96a12d51e032cbb1967a2d41e89cd26d21fa454
VITALIJ <Vitaliythesecond@gmail.com>
2022-02-21 21:26:38 +0300

    recaptcha initial unmount

--- src/hook/useInitialCaptcha.ts
+++ src/hook/useInitialCaptcha.ts
@@ -1,9 +1,18 @@
 import { useEffect } from "react";
 
 export const useGetInitialCaptcha = (key: string) => {
-   useEffect(() => {
+useEffect(() => {
         const script = document.createElement("script");
         script.src = "https://www.google.com/recaptcha/api.js?render=" + key;
+        script.id = "g-recaptcha-id";
         document.body.appendChild(script);
+
+        // if we need recaptcha on the single page
+        return () => {
+            const scriptElem = document.getElementById("g-recaptcha-id");
+            scriptElem!.remove();
+            const recaptchaElems = document.getElementsByClassName("grecaptcha-badge");
+            recaptchaElems[0].remove();
+        };
     }, [key]);
 };
